d91edfff19ec901cb9da235f900c033dba3648e8
yogesh <gahlawatyogesh0@gmail.com>
2026-08-02 18:16:48 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter principal amount: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter annual interest rate (%): ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter time (in years): ");
+        double time = sc.nextDouble();
+
+        double simpleInterest = principal * rate * time / 100;
+        double finalAmount = principal + simpleInterest;
+
+        System.out.println("Simple Interest = " + simpleInterest);
+        System.out.println("Final Amount = " + finalAmount);
+
+        sc.close();
+    }
+}
